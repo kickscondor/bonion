5e42166bb3b7c9308aa0ff4ced8ccba0cec4fa99
kickscondor <kicks@kickscondor.com>
2020-06-10 06:38:26 +0000

    Added getLeastCommonFork, isMain, canWrite and such.

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ to merge changes.
 
 Here's how it goes:
 
-1. Create the master hyperdrive. (This could even be a copy of another bonion.)
+1. Create the primary hyperdrive. (This could even be a copy of another bonion.)
 2. Add forked hyperdrives by mounting them under a `.forks` directory at the
    root of the drive.
 3. Mark deleted files by adding a metadata entry to the file named `bonion`
@@ -20,18 +20,54 @@ details that make the union possible.
 
 ## Discovering Forks
 
-From the master hyperdrive, one simply needs to list the contents of the 
+From the primary hyperdrive, one simply needs to list the contents of the 
 `.forks` directory to see the approved forks.
 
-     master (hyper://1c6d8c..c4/)
-       ┣━━ .forks
+     primary (hyper://1c6d8c..c4/)
+       ┗━━ .forks
              ┣━━ friendo (hyper://abcdef..01/)
              ┣━━ pal (hyper://abcdef..02/)
              ┗━━ forkling (hyper://abcdef..03/)
 
 However, from a fork, the `forkOf` property of the `index.json` contains
-the address to the master hyperdrive.
-So that drive is added to the union - as well as any `.forks` (and any master
-drive and its forks, etc) attached.
+the address to the primary hyperdrive. So that drive is added to the union - as
+well as any `.forks` (and any primary drive and its forks, etc) attached.
+
+## Seeding Forks
+
+One issue with seeding union mounts is that usually everyone will seed the
+primary hyperdrive - it's the one that gets linked to. This is an issue because
+the other drives need to be seeded equally - they all ultimately get accessed
+on every request.
+
+One possible way of dealing with this is to redirect visitors to one of the
+forks - almost like you would with load-balancing on HTTP. This process could
+look like this:
+
+1. If a 'favoriteFork' cookie is found, redirect to that fork, if necessary.
+2. Otherwise, get a list of all forks, along with the peer count.
+3. Save the address of the least-common fork (so long as it's not zero) to
+   'favoriteFork', then redirect.
+
+Of course, this could be an issue if the least-common fork is inaccessible - so
+you'll need to be sure you have at least one solid seed for a fork.
+
+Here's some sample code illustrating this process, using the
+`getLeastCommonFork` method:
+
+     let B = await bonion(beaker.hyperdrive.drive())
+     if (B.isMain()) {
+       let fave = localStorage.getItem('favoriteFork')
+       if (!fave) {
+         let lcf = await B.getLeastCommonFork()
+         if (lcf) {
+           fave = lcf.drive.url
+           localStorage.setItem('favoriteFork', fave)
+         }
+       }
+       if (fave) {
+         window.location.replace(fave)
+       }
+     }
 
 ## API
--- bonion.js
+++ bonion.js
@@ -2,6 +2,12 @@
 // bonion
 // Beaker union mounts w/o conflict resolution.
 //
+
+//
+// bonion(drive[, opts]) => [object Bonion]
+// - drive: A hyperdrive object. (e.g. beaker.hyperdrive.drive())
+// - opts: {depth: N} how deeply will the fork search for child forks?
+//
 module.exports = async (...args) => {
   let bon = new Bonion(...args)
   await bon.scan()
@@ -11,6 +17,7 @@ module.exports = async (...args) => {
 class Bonion {
   constructor (drive, opts = {}) {
     this.drive = drive
+    this.index = null
     Object.assign(this.opts = {depth: 1}, opts)
     this.forks = {}
   }
@@ -19,12 +26,12 @@ class Bonion {
     if (!drive)
       drive = this.drive
     if (depth > 20)
-      throw new InternalError("the fork tree is too large, more than 20 master hyperdrives")
+      throw new InternalError("the fork tree is too large, more than 20 primary hyperdrives")
 
     let current = this.forks[drive.url]
     if (current) {
       //
-      // If a master drive contains a fork, delete our duplicate in the current
+      // If a primary drive contains a fork, delete our duplicate in the current
       // drive.
       //
       if (depth === 0 && current.drive === this.drive && drive !== this.drive) {
@@ -34,7 +41,7 @@ class Bonion {
         current.label = label
       return
     }
-    this.forks[drive.url] = {drive, label}
+    this.forks[drive.url] = current = {drive, label}
 
     //
     // Access all forks
@@ -50,25 +57,91 @@ class Bonion {
     }
 
     //
-    // Access master drive and its forks
+    // Access primary drive and its forks
     //
     try {
-      info = JSON.parse(await drive.readFile('index.json'))
-      if (info.forkOf) {
-        this.scan(beaker.hyperdrive.drive(info.forkOf), null, depth)
+      current.index = index = JSON.parse(await drive.readFile('index.json'))
+      if (drive === this.drive) {
+        this.index = index
+      }
+      if (index.forkOf) {
+        this.scan(beaker.hyperdrive.drive(index.forkOf), null, depth)
       }
     } catch {}
   }
 
+  //
+  // Get this drive's info.
+  //
+  async getInfo() {
+    let d = this.forks[this.drive.url]
+    if (!d.info) {
+      d.info = await this.drive.getInfo()
+    }
+    return d.info
+  }
+
+  //
+  // Is the drive we're on the main drive?
+  //
+  isMain() {
+    return typeof(this.index) === 'object' && typeof(this.index.forkOf) === 'undefined'
+  }
+
+  //
+  // Are we an owner of any of the forks?
+  //
+  async canWrite() {
+    for (let url in this.forks) {
+      let fork = this.forks[url]
+      if (!fork.info) {
+        fork.info = await fork.drive.getInfo()
+      }
+      if (fork.info.writable) {
+        return true
+      }
+    }
+    return false
+  }
+
+  //
+  // Add a mount under .forks for the given drive.
+  //
   async addFork(label, url) {
     await this.drive.mkdir('/.forks')
     await this.drive.mount(`/.forks/${label}`, url)
   }
 
+  //
+  // Remove a mount under .forks named `label`.
+  //
   async removeFork(label) {
     await this.drive.unmountFork(`/.forks/${label}`)
   }
 
+  //
+  // Get the least-common fork. (Available fork with the fewest seeds.)
+  //
+  async getLeastCommonFork() {
+    let acc = 0, lcf = null
+    for (let url in this.forks) {
+      let fork = this.forks[url]
+      if (!fork.info) {
+        fork.info = await fork.drive.getInfo()
+      }
+
+      let peers = fork.info?.peers || 0
+      if (acc == 0 || (peers != 0 && peers < acc)) {
+        acc = peers
+        lcf = fork
+      }
+    }
+    return lcf
+  }
+
+  //
+  // Build a directory listing by reading all of the attached forks.
+  //
   async readdir(url) {
     //
     // Build an array of [listing, drive object] (or null
@@ -100,6 +173,9 @@ class Bonion {
     return Object.values(recent).sort((a, b) => a.name.localeCompare(b.name))
   }
 
+  //
+  // Find the most recent file among the attached forks.
+  //
   async stat(url, opts = null) {
     opts = opts || {}
     //
@@ -120,6 +196,9 @@ class Bonion {
     return st
   }
 
+  //
+  // URL for Beaker's merge page, comparing a fork to the current drive.
+  //
   mergeUrl(drive) {
     if (this.drive.url !== drive.url) {
       return `beaker://diff/?base=${encodeURIComponent(drive.url)}&target=${encodeURIComponent(this.drive.url)}`
